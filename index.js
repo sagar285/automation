329f04b68c75b9c54b733ec9138c1ed8b68f8d0c
sagar <anandguptasir@gmail.com>
2025-04-01 18:10:55 +0530

    coomit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// Verification endpoint
+app.get('/webhook', (req, res) => {
+    console.log(req.body,"pppp");
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+  
+  // Your verification token (set this in Facebook Developer Console)
+  const VERIFY_TOKEN = 'IGAApOtLQdo9FBZAE5hVWRrQTEzYUN6WFhNQmtseVVXdDRGS29iUFlxS1N2dGdMLU5XMDlqMk81MDl1S2dMT3M1NVljRXpJU3VxZAmdnRmxJLXFuYjVna0V4UzVfSWdPb3c1Y2ZAyTE9QdS1scVhxOUM2QUkwMVFGaE1ldTBQc1o2QQZDZD';
+  
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    console.error('Verification failed');
+    res.sendStatus(403);
+  }
+});
+
+// Webhook event handler
+app.post('/webhook', (req, res) => {
+  console.log('Webhook event received:', req.body);
+  // Process the webhook event here
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server is running'));
