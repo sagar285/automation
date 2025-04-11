3182a3e44c7db08681ba3380e62f0fa0d883c522
sagar <anandguptasir@gmail.com>
2025-04-12 00:28:03 +0530

    mmm

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ dotenv.config({})
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', // Your frontend URL
+  // origin: 'http://localhost:3000', // Your frontend URL
+  origin: 'https://fliqrai.vercel.app', // Your frontend URL
   credentials: true 
 }))
 app.use(router);
